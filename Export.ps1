7b237cc05bdc677ea5065ce5e20bff23bcc97927
Philip Wales <ptwales@gmail.com>
2015-06-02 17:25:35 -0500

    Finished building Export

--- Export.ps1
+++ Export.ps1
@@ -10,16 +10,45 @@ Param(
 )
 
 function main {
-   $fileCOM = (OpenOfficeFile $sourceFile)
-   $prjCOM = ($fileCOM.VBProject)
-   ExportModules $prjCOM $outDest
+	$officeCOM = (OfficeComFromFileName $sourceFile)
+	$fileCOM = (OpenMSOfficeFile $officeCOM $sourceFile)
+	$prjCOM = ($fileCOM.VBProject)
+	ExportModules $prjCOM $outDest
+	$officeCOM.Quit()
 }
 function OfficeComFromFileName([String] $fileName) {
+
+	$fileExt = [System.IO.Path]::GetExtension($fileName)
+	$officeCOM = switch -wildcard ($fileExt.ToLower()) {
+        ".xl*" {New-Object -ComObject Excel.Application; break}
+        ".ac*" {throw "Access is not yet supported"; break} #{New-Object -ComObject Acces.Application; break}
+        default {throw "$fileName is not a supported office file."}
+    }
+    return $officeCOM
 }
-# not the FOSS OpenOffice
-function OpenOfficeFile([String] $sourceFile) {
+function OpenMSOfficeFile($officeCOM, [String] $filePath) {
+	$fileCOM = ($officeCOM.Workbooks.Open($filePath))
+	return $fileCOM
 }
-function ExportModules ($prjCOM, [String] $outDest) {
+function ExportModules($prjCOM, [String] $outDest) {
+    
+	$vbComps = ($prjCOM.VBComponents)
+	ForEach ($component in $vbComps) {
+		$compFileExt = (GetComponentExt($component))
+		if ($compFileExt -ne "") {
+			$compFileName = $component.name + $compFileExt
+			$exportPath = (Join-Path $outDest $compFileName)
+			$component.Export($exportPath)
+    	}
+	}
 }
-#entry point
-main
+function GetComponentExt($component) {
+	$compExt = switch ($component.Type) {
+		1 {".bas"}
+		2 {".cls"}
+		# form
+		default {""}
+	}
+	return $compExt
+}
+main # entry point
