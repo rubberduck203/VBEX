e3288841c73c5ed4ee8b6a1bb6dc5e229a904f7f
Philip Wales <ptwales@gmail.com>
2015-06-08 10:31:53 -0500

    Compatible with Powershell < 3

--- Make.ps1
+++ Make.ps1
@@ -12,15 +12,20 @@ Param(
 # locations of required libraries change according to OS arch.
 # Our libs are 32 bit.
 $programFiles = if ([Environment]::Is64BitOperatingSystem) { 
-	"Program Files (x86)"
+    "Program Files (x86)"
 } else { 
-	"Program Files"
+    "Program Files"
 }
-
+# Compatible with earlier powershell versions
+$scriptRoot = if ($PSVersionTable.PSVersion.Major -ge 3) {
+    $PSScriptRoot
+} else {
+    Split-Path $MyInvocation.MyCommand.Path -Parent
+}
 # constants are illegible in powershell
 $VBA_EXTENSIBILITY_LIB = "C:\$programFiles\Common Files\Microsoft Shared\VBA\VBA6\VBE6EXT.OLB"
 $VBA_SCRIPTING_LIB = "C:\Windows\system32\scrrun.dll"
-$buildScript = (Join-Path $PSScriptRoot "Build.ps1")
+$buildScript = (Join-Path $scriptRoot "Build.ps1")
 
 $buildRefs = @{
     "src" = @("$VBA_EXTENSIBILITY_LIB", "$VBA_SCRIPTING_LIB");
@@ -28,8 +33,8 @@ $buildRefs = @{
 }
 
 ForEach ($build In $buildRefs.Keys) {
-    $path = (Join-Path $PSScriptRoot "VBEX$build.xlam")
-    $files = (Get-ChildItem (Join-Path $PSScriptRoot $build)).FullName
+    $path = (Join-Path $scriptRoot "VBEX$build.xlam")
+    $files = (Get-ChildItem (Join-Path $scriptRoot $build)).FullName
     $refs = $buildRefs[$build]
     & "$buildScript" "$path" $files $refs
 }
