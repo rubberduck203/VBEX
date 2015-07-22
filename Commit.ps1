74635edf332d828bbc9036b1b41dc3adefe73dd9
unknown <ptwales@openmailbox.org>
2015-07-22 16:23:46 -0500

    modified script root var to compat with ps1

--- Commit.ps1
+++ Commit.ps1
@@ -1,9 +1,14 @@
-$export = (Join-Path "$PSScriptRoot" "Export.ps1")
+$scriptRoot = if ($PSVersionTable.PSVersion.Major -ge 3) {
+    $PSScriptRoot
+} else {
+    Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+$export = (Join-Path "$scriptRoot" "Export.ps1")
 $builds = @("src", "test")
 
 ForEach($build in $builds) {
-    $file = (Join-Path "$PSScriptRoot" "VBEX$build.xlam")
-    $dest = (Join-Path "$PSScriptRoot" "$build")
+    $file = (Join-Path "$scriptRoot" "VBEX$build.xlam")
+    $dest = (Join-Path "$scriptRoot" "$build")
     Get-ChildItem "$dest" | Remove-Item
     & $export "$file" "$dest"
 }
