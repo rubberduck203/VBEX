b7e9b1b0ada1097afe86fac3cb841188753e6705
Philip Wales <ptwales@openmailbox.org>
2015-11-10 10:51:29 -0600

    Export deletes files and prints messages

--- Export.ps1
+++ Export.ps1
@@ -10,14 +10,17 @@ Param(
 )
 
 function main {
+        Write-Host "Will Export $sourceFile to $outDest`:"
 	$officeCOM = (OfficeComFromFileName $sourceFile)
 	$fileCOM = (OpenMSOfficeFile $officeCOM $sourceFile)
 	$prjCOM = ($fileCOM.VBProject)
 	ExportModules $prjCOM $outDest
 	$officeCOM.Quit()
+        Start-Sleep -seconds 1
+        Write-Host "Removing $sourceFile" -ForeGround Red
+        Remove-Item $sourceFile
 }
 function OfficeComFromFileName([String] $fileName) {
-
 	$fileExt = [System.IO.Path]::GetExtension($fileName)
 	$officeCOM = switch -wildcard ($fileExt.ToLower()) {
         ".xl*" {New-Object -ComObject Excel.Application; break}
@@ -27,19 +30,23 @@ function OfficeComFromFileName([String] $fileName) {
     return $officeCOM
 }
 function OpenMSOfficeFile($officeCOM, [String] $filePath) {
+        Write-Host "Opening $filePath with Office Application."
 	$fileCOM = ($officeCOM.Workbooks.Open($filePath))
 	return $fileCOM
 }
 function ExportModules($prjCOM, [String] $outDest) {
     
 	$vbComps = ($prjCOM.VBComponents)
+        $count = $vbComps.count
+        Write-Host "Exporting $count modules:"
 	ForEach ($component in $vbComps) {
 		$compFileExt = (GetComponentExt($component))
 		if ($compFileExt -ne "") {
 			$compFileName = $component.name + $compFileExt
 			$exportPath = (Join-Path $outDest $compFileName)
+                        Write-Host "`t $compFileName `t => `t $exportPath"
 			$component.Export($exportPath)
-    	}
+                }
 	}
 }
 function GetComponentExt($component) {
